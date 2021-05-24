57d276cf51122fc836eda75cb33f77583e76888e
James Kominick <james@kominick.com>
2021-05-23 23:26:47 -0400

    support post-slack-login redirects
    If a login is initiated using `/login?redirect=/status`, then
    after the slack auth process is complete you'll be redirected
    to `/status`

--- src/slackat/handlers.clj
+++ src/slackat/handlers.clj
@@ -7,37 +7,50 @@
             [aleph.http :as http]
             [java-time :as jt]
             [clojure.java.jdbc :as j]
-            [clojure.java.io :as io]
             [clojure.core.async :as a]
             [clojure.core.cache :as cache]
+            [clojure.string :as str]
             [cheshire.core :as json]
             [slackat.execution :as ex]
             [slackat.database.core :as db]
             [slackat.crypto :as cr]
             [slackat.utils :as u :refer [->resp ->text ->json ->redirect]]
             [slackat.slack :as slack]
-            [slackat.config :as config]))
+            [slackat.config :as config])
+  (:import (java.time Instant)
+           (java.sql Timestamp)))
 
 
-(defn -user-for-req [req]
+(defn user-for-req [req]
   "load a user based on their auth token, or null"
-  (u/spy req :on-index)
+  ;(u/spy req :on-index)
   (let [cookie (u/get-some-> req :headers :cookie)
         [_ token] (some->> cookie (re-find #"auth_token=(\w+)"))
-        user-id (some->> token cr/sign (db/get-user-for-auth (db/conn)))]
-    user-id))
+        user-auth (some->> token cr/sign (db/get-user-for-auth (db/conn)))
+        user-id (some-> user-auth :id)
+        ^Instant created (some-> user-auth :created ((fn [^Timestamp ts] (.toInstant ts))))
+        ^Instant month-ago (jt/instant (jt/minus (u/utc-now) (jt/days 30)))]
+    (if (or (nil? created) (.isBefore created month-ago))
+      (do
+        (t/info "cookie expired" {:created created :user-id user-id})
+        nil)
+      user-id)))
 
 
 (defn index [req]
-  (d/let-flow [user (d/future (-user-for-req req))]
+  (d/let-flow [user (d/future (user-for-req req))]
               (if (some? user)
                 (->text "Welcome back!")
                 (->redirect "/login?redirect=/"))))
 
-;; 2m cache ttl
+
+;; -- slack authentication unique state token store
+;; 2m ttl of one-time state-tokens used for slack logins
 (defonce state-tokens (atom (cache/ttl-cache-factory {} :ttl 120000)))
 
-(defn new-state-token []
+(defn new-state-token
+  "Make a new one-time state-token and cache it temporarily"
+  []
   (-> (cr/rand-bytes 32)
       buddy.core.codecs/bytes->hex
       ((fn [hex]
@@ -46,7 +59,10 @@
                                (cache/through-cache s hex (constantly nil))))
          hex))))
 
-(defn valid-state-token? [token]
+(defn valid-state-token?
+  "Check if a state-token is still valid (still cached).
+  Expire the token so it can't be used again."
+  [token]
   (let [valid (atom false)]
     (swap!
       state-tokens
@@ -60,11 +76,35 @@
     @valid))
 
 
+;; -- formatting and parsing state tokens used for stripe logins
+(defn format-state-str
+  "Format a state-token and metadata map as
+  $state-token.$metadata-as-b64"
+  ([state-token] (format-state-str state-token {}))
+  ([state-token metadata]
+   (let [metadata (or metadata {})
+         meta-str (u/map->b64-str metadata)]
+     (format "%s.%s" state-token meta-str))))
+
+(defn parse-state-str
+  "Parse a formatted state token string into the
+  separate state-token and metadata parts"
+  [state-str]
+  (let [[state-token metadata] (str/split state-str #"\.")]
+    (if (or (nil? state-token) (nil? metadata))
+      (u/ex-error! (format "Found invalid state token in slack login redirect: %s" state-str))
+      [state-token (u/b64-str->map metadata)])))
+
+
 (defn login
-  "redirect to slack to login"
+  "Initiate slack login/oauth request.
+  Redirect to slack's auth endpoint with our state and metadata."
   [req]
-  (-> (slack/build-redirect (new-state-token))
-      ->redirect))
+  ;(u/spy req :on-login)
+  (let [redirect (u/get-some-> req :query-params "redirect")
+        state-str (format-state-str (new-state-token) {"redirect" redirect})]
+    (-> (slack/build-redirect state-str)
+        ->redirect)))
 
 
 ;; ------ example:
@@ -84,7 +124,10 @@
 ;  "ok" true,
 ;  "team" {"id" "TTT", "name" "THE TEAM"},
 ;  "app_id" "APPP"}}
-(defn -login-user [slack-auth-info]
+(defn save-user-get-cookie
+  "Given slack auth info about a user, upsert a user and
+  their api tokens and cookie in the db, return the new auth cookie"
+  [slack-auth-info]
   (let [sla slack-auth-info
         bot-id (sla "bot_user_id")
         bot-token (sla "access_token")
@@ -117,11 +160,11 @@
                                          :slack-id user-id
                                          :slack-team-id team-id
                                          :scope user-scope})]
-          (u/spy user :after-login)
+          ;(u/spy user :after-login)
           {:auth-token auth-token}))))
 
 
-(defn -build-cookie [auth-token]
+(defn build-cookie [auth-token]
   (format
     "auth_token=%s; Secure; HttpOnly; Max-age=%s; Path=/; Domain=%s; SameSite=Lax"
     auth-token
@@ -132,23 +175,31 @@
 (defn login-slack
   "redirected here from slack's authorization"
   [req]
-  (let [{:strs [code state]} (req :params)]
+  ;(u/spy req :on-slack-redirect)
+  (let [{:strs [code state]} (req :params)
+        [state metadata] (parse-state-str state)]
     (if (nil? code)
       (->text "Login failed. Please try logging in again")
       (if (not (valid-state-token? state))
         (->text "Login redirect token expired. Please try logging in again")
         (d/chain
+          ;; get the user info and api token for this user
           (slack/exchange-access code)
-          #(u/spy % :after-exchange)
-          (fn [auth-info] (d/future (-login-user auth-info)))
+          ;#(u/spy % :after-exchange)
+
+          ;; upsert this user's (and team's) api tokens
+          (fn [auth-info] (d/future (save-user-get-cookie auth-info)))
+
+          ;; generate a new auth cookie and handle any post-login redirects
           (fn [{auth-token :auth-token}]
-            (let [cookie (-build-cookie auth-token)]
-              ;; todo: handle ?redirect= query param that we set when we redirected
-              ;;       to our internal /login endpoint. This means that we need to
-              ;;       update the slack-redirect "state" to be a composite key that
-              ;;       has some base64 data encoded on the end of it.
-              (->text "Welcome!"
-                      :headers {:set-cookie cookie}))))))))
+            (let [cookie (build-cookie auth-token)
+                  headers {:set-cookie cookie}]
+              ;(u/spy metadata :state-metadata)
+              (if-let [redirect (u/get-some-> metadata "redirect")]
+                ;; handle ?redirect= query param that we set when we did our initial _internal_
+                ;; redirect from some page (e.g. /) to /login?redirect=/
+                (->redirect redirect :headers headers)
+                (->text "Welcome!" :headers headers)))))))))
 
 
 (defn parse-cmd [command]
